b938c8926f18dbf56eb37d8cee5d2ddc72b74926
Rachata Triratjarasporn <64809413+TemptingForza@users.noreply.github.com>
2024-07-26 19:33:30 +1200

    12th Commit
    Have made a contact us page will need to research how that messages will reach us.

--- Scripts/Sidebar.js
+++ Scripts/Sidebar.js
@@ -1,8 +1,8 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
 }
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
 }
